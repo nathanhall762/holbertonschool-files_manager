71c6824b3cfc5b90b716e21c12f7a8cec01c793d
nathanhall762 <nathanhall762@gmail.com>
2023-07-19 11:51:27 -0500

    task 1

--- main.js
+++ main.js
@@ -1,60 +1,30 @@
-import MongoClient from 'mongodb';
-import chai from 'chai';
-import dbClient from './utils/db.js';
+import dbClient from './utils/db';
 
-
-describe('dbClient test', () => {  
-    let testClientDb = null;
-
-    beforeEach(async () => {
-        const dbInfo = {
-            host: process.env.DB_HOST || 'localhost',
-            port: process.env.DB_PORT || '27017',
-            database: process.env.DB_DATABASE || 'files_manager'
-        };
-        const mcPromise = () => {
-            return new Promise((resolve, reject) => {
-                MongoClient.connect(`mongodb://${dbInfo.host}:${dbInfo.port}/${dbInfo.database}`, (err, client) => {
-                    if (err) {
-                        reject();
-                    }
-                    else {
-                        resolve(client.db(dbInfo.database))
-                    }
-                });
-            }); 
-        };
-        testClientDb = await mcPromise();
-        await testClientDb.collection('users').deleteMany({})
-        await testClientDb.collection('users').insertOne({ email: "me@me.com" })
-
-        const waitConnection = () => {
-            return new Promise((resolve, reject) => {
-                let i = 0;
-                const repeatFct = async () => {
-                    await setTimeout(() => {
-                        i += 1;
-                        if (i >= 5) {
-                            reject()
-                        }
-                        else if(!dbClient.isAlive()) {
-                            repeatFct()
-                        }
-                        else {
-                            resolve()
-                        }
-                    }, 1000);
+const waitConnection = () => {
+    return new Promise((resolve, reject) => {
+        let i = 0;
+        const repeatFct = async () => {
+            await setTimeout(() => {
+                i += 1;
+                if (i >= 10) {
+                    reject()
                 }
-                repeatFct();
-            })
+                else if(!dbClient.isAlive()) {
+                    repeatFct()
+                }
+                else {
+                    resolve()
+                }
+            }, 1000);
         };
-        await waitConnection();
-    });
-        
-    afterEach(async () => {
-    });
-
-    it('nbUsers for a collection with one document', async () => {
-        chai.assert.equal(await dbClient.nbUsers(), 1);  
+        repeatFct();
     })
-});
+};
+
+(async () => {
+    console.log(dbClient.isAlive());
+    await waitConnection();
+    console.log(dbClient.isAlive());
+    console.log(await dbClient.nbUsers());
+    console.log(await dbClient.nbFiles());
+})();
--- utils/db.js
+++ utils/db.js
@@ -5,14 +5,18 @@ class DBClient {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-
-    const url = `mongodb://${host}:${port}`;
+    const url = `mongodb://${host}:${port}/${database}`;
 
     this.client = new MongoClient(url, { useUnifiedTopology: true });
+  }
 
-    this.isAlive = false; // Initialize isAlive to false
-
-    this.connect();
+  async isAlive() {
+    try {
+      await this.client.connect();
+      return true;
+    } catch (error) {
+      return false;
+    }
   }
 
   async connect() {
@@ -26,28 +30,30 @@ class DBClient {
   }
 
   async nbUsers() {
-    if (!this.isAlive) {
-      throw new Error('Database connection is not alive.');
+    try {
+      await this.client.connect();
+      const db = this.client.db();
+      const usersCollection = db.collection('users');
+      const numUsers = await usersCollection.countDocuments();
+      return numUsers;
+    } catch (error) {
+      return 0;
     }
-
-    const db = this.client.db();
-    const collection = db.collection('users');
-    const count = await collection.countDocuments();
-    return count;
   }
 
   async nbFiles() {
-    if (!this.isAlive) {
-      throw new Error('Database connection is not alive.');
+    try {
+      await this.client.connect();
+      const db = this.client.db();
+      const filesCollection = db.collection('files');
+      const numFiles = await filesCollection.countDocuments();
+      return numFiles;
+    } catch (error) {
+      return 0;
     }
-
-    const db = this.client.db();
-    const collection = db.collection('files');
-    const count = await collection.countDocuments();
-    return count;
   }
 }
 
 const dbClient = new DBClient();
 
-module.exports = { dbClient };
+module.exports = dbClient;
